f00d9eee87cf4393d68b8eccb6db73b25d91af1c
Ackim59 <mickael.vandev@gmail.com>
2022-03-28 14:09:05 +0200

    Ajout de l'import des données depuis l'API d'OpenWeatherMap et affichage des données brutes dans index.html

--- app/bike_app/views.py
+++ app/bike_app/views.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, url_for
+import requests
 
 app = Flask(__name__)
+
+app.config.from_object('config')
 
 @app.route('/')
 @app.route('/index/')
 def index():
-     return render_template('index.html')
+     API_KEY_OPENWEATHERMAP = app.config['API_KEY_OPENWEATHERMAP']
+     LAT = 50.62925
+     LONG = 3.057256
+     url = 'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&appid={}'.format(LAT, LONG, API_KEY_OPENWEATHERMAP)
+     req = requests.get(url)
+     data = req.content
+     return render_template('index.html', data=data)
 
 @app.route('/predict/')
 def predict():

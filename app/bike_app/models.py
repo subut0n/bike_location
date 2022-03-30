29c2df80466d9a26e545fddabc0d51cdeadac258
Adrien Dzikowski <subut0n@mac-mini-de-adrien.home>
2022-03-30 09:08:22 +0200

    morning push

---
+++ app/bike_app/models.py
@@ -0,0 +1,12 @@
+class FuturePrediction():
+    """Create a table Prediction on the database
+    Args:
+    """
+    date = 
+    weather = 
+    registered =
+    casual =
+    count =
+
+class TodayPrediction():
+    """Create Prediction from """

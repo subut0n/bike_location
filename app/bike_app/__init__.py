772e85a87142c756285594587fb10e6075d954bf
Ackim59 <mickael.vandev@gmail.com>
2022-03-30 17:01:47 +0200

    onehotencodage des données de l'API + ajout de la fonction de prédiction.

--- app/bike_app/__init__.py
+++ app/bike_app/__init__.py
@@ -1,6 +1,4 @@
 from flask import Flask
-import pandas as pd
 
 from .views import app
 
-data = pd.read_csv('..\\csv\\train_modifie.csv')
--- app/bike_app/utils.py
+++ app/bike_app/utils.py
@@ -1,6 +1,9 @@
 import requests
 import json
 from datetime import datetime
+import pickle as pkl
+import pandas as pd
+import ast
 
 def load_dataAPI(lat, long, api_key):
      url = 'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&appid={}'.format(lat, long, api_key)
@@ -8,64 +11,86 @@ def load_dataAPI(lat, long, api_key):
      req = req.content.decode("utf-8")
      return json.loads(req)
 
-def get_48h_data(dataAPI):
+def get_48h_data(dataAPI, feature_names):
     dataAPI = [dataAPI[i] for i in range(len(dataAPI))]
     data = []
     for i, dict in enumerate(dataAPI):
-        data.append(data_formatting(dict))
+        data.append(data_formatting(dict, feature_names))
     return f'{data}'
 
 
-def data_formatting(currentDataAPI):
+def data_formatting(currentDataAPI, feature_names):
+     # keys correspond aux clés du dataset après onehotencodage
 
-    # On extrait les données nécessaires pour la prédiction
-    time_stamp = currentDataAPI['dt']
-    currentDataAPI['dt'] = datetime.fromtimestamp(currentDataAPI['dt'])
+     # On extrait les données nécessaires pour la prédiction
+     time_stamp = currentDataAPI['dt']
+     currentDataAPI['dt'] = datetime.fromtimestamp(currentDataAPI['dt'])
+     currentDataAPI['weather'] = currentDataAPI['weather'][0]['id']
 
-    currentDataAPI['weather'] = currentDataAPI['weather'][0]['id']
-    keys = ['dt', 'temp', 'feels_like', 'humidity', 'wind_speed', 'weather']
-    new_keys = ['dt', 'temp', 'atemp', 'humidity', 'windspeed', 'weather']
-    current = {}
-    for key, new_key in zip(keys, new_keys):
-        current[new_key] = currentDataAPI[key]
+     # Création d'un dictionnaire avec les clés correspondant aux noms des features utilisées
+     # lors de la modélisation donc encodées.
+     current_dict = {}
+     for name in feature_names:
+          current_dict[name] = 0
 
-    current['temp'] = current['temp'] - 273.15
-    current['atemp'] = current['atemp'] - 273.15
+     keys = ['dt', 'temp', 'feels_like', 'humidity', 'wind_speed', 'weather']
+     new_keys = ['dt', 'temp', 'atemp', 'humidity', 'windspeed', 'weather']
+     current = {}
+     for key, new_key in zip(keys, new_keys):
+          current[new_key] = currentDataAPI[key]
+     current['temp'] = current['temp'] - 273.15
+     current['atemp'] = current['atemp'] - 273.15
 
-    weather_1 = [800, 801, 802]
-    weather_2 = [701, 711, 721, 803, 804]
-    weather_3 = [200, 201, 300, 301, 310,
-                 500, 501, 520, 600, 601, 611, 612, 615, 620,
-                 731, 741, 751]
-    weather_4 = [202, 210, 211, 212, 221, 230, 231, 232,
-                 301, 302, 311, 312, 313, 314, 321,
-                 502, 503, 504, 511, 520, 521, 522, 531,
-                 602, 613, 616, 621, 622, 761, 762, 771, 781]
-    weathers = [weather_1, weather_2, weather_3, weather_4]
-    
-    for i, weather in enumerate(weathers):
-        if current['weather'] in weather:
-            current['weather'] = i+1
+     for key in current.keys():
+          current_dict[key] = current[key]
 
-    current['hours'] = current['dt'].hour
-    current['week_days'] = datetime.fromtimestamp(time_stamp).weekday() + 1
-    current['months'] = current['dt'].month
-    current['years'] = current['dt'].year
+     weather_1 = [800, 801, 802]
+     weather_2 = [701, 711, 721, 803, 804]
+     weather_3 = [200, 201, 300, 301, 310,
+                    500, 501, 520, 600, 601, 611, 612, 615, 620,
+                    731, 741, 751]
+     weather_4 = [202, 210, 211, 212, 221, 230, 231, 232,
+                    301, 302, 311, 312, 313, 314, 321,
+                    502, 503, 504, 511, 520, 521, 522, 531,
+                    602, 613, 616, 621, 622, 761, 762, 771, 781]
+     weathers = [weather_1, weather_2, weather_3, weather_4]
+     weather_labels = ['weather_1', 'weather_2', 'weather_3', 'weather_4']
+     for i, weather in enumerate(weathers):
+          if current_dict['weather'] in weather:
+               current_dict[weather_labels[i]] = i+1
+     current_dict.pop('weather')
 
-    seasons = ['06-21', '09-21', '12-21']
-    for i, season in enumerate(seasons):
-        if (current['dt'].strftime('%m-%d') < '03-21'):
-            current['season'] = 4
-            break
-        if ((current['dt'].strftime('%m-%d') < season) & (current['dt'].strftime('%m-%d') >= '03-21')):
-            current['season'] = i+1
-            break
-    
-    if (current['week_days'] >= 6):
-        current['holiday'] = 0
-        current['workingday'] = 0
-    else:
-        current['holiday'] = 0
-        current['workingday'] = 1
+     current_dict['hours'] = current_dict['dt'].hour
+     current_dict['years'] = current_dict['dt'].year
 
-    return current
+     month = current_dict['dt'].month
+     month_label = 'months_' + str(month)
+     current_dict[month_label] = 1
+
+     week_day = datetime.fromtimestamp(time_stamp).weekday()+1
+     week_day_label = 'week_days_' + str(week_day)
+     current_dict[week_day_label] = 1
+
+     seasons = ['06-21', '09-21', '12-21']
+     for i, season in enumerate(seasons):
+          if (current_dict['dt'].strftime('%m-%d') < '03-21'):
+               current_dict['season_4'] = 1
+               break
+          if ((current_dict['dt'].strftime('%m-%d') < season) & (current_dict['dt'].strftime('%m-%d') >= '03-21')):
+               current_dict['season_' + str(i+1)] = 1
+               break
+     
+     if (week_day >= 6):
+          current_dict['holiday'] = 0
+          current_dict['workingday'] = 0
+     else:
+          current_dict['holiday'] = 0
+          current_dict['workingday'] = 1
+     current_dict.pop('dt')
+     return current_dict
+
+def prediction(pickle_uri, data):
+     with open(pickle_uri, 'rb') as pickle_file:
+          model = pkl.load(pickle_file)
+     data = pd.DataFrame(ast.literal_eval(data))
+     return f'{model.predict(data)}'
